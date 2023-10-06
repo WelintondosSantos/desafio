d1466df30dbb0f5c71716b62e26b05fc76f035e6
Welinton1983 <wellsilva1000@outlook.com.br>
2023-10-06 19:03:52 -0300

    desafio v1

--- desafio.js
+++ desafio.js
@@ -1,1 +1,42 @@
-console.log("Oi");
+var heroi = "Lionel Messi"
+var pontosXp = 10000
+
+switch (true) {
+  case  pontosXp <= 1000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Ferro! "); 
+  break;
+  case  pontosXp > 1001 && pontosXp <= 2000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Bronze! "); 
+  break;
+  case  pontosXp > 2001 && pontosXp <= 5000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Prata! "); 
+  break;
+  case  pontosXp > 6001 && pontosXp <= 7000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Ouro! "); 
+  break;
+  case  pontosXp > 7001 && pontosXp <= 8000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Platina! "); 
+  break;
+  case  pontosXp > 8001 && pontosXp <= 9000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Ascendente! "); 
+  break;
+  case  pontosXp > 9001 && pontosXp <= 10.000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Imortal! "); 
+  break;
+  case  pontosXp > 10.000:
+  console.log(" O herói " + heroi  + " tem " + pontosXp  + " xp e está no nível Radiante! "); 
+  break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
